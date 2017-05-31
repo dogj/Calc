cb69c04a1d3588389297ad7be0b00fb3cd849348
dogj <924625141@qq.com>
2017-05-31 23:49:00 +1200

    add some pages and pictures

---
+++ app/src/main/java/com/jiangxin/calc/FormulaSearch.java
@@ -0,0 +1,13 @@
+package com.jiangxin.calc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormulaSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formula_search);
+    }
+}
